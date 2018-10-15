dfa8808a819ec8a60a89d2e4669e90d34807dd7b
Prachi <prachi@hackcapital.com>
2018-10-15 08:33:17 -0700

    minor change in comments

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,9 +7,8 @@ import (
 // Home : Handler for "/" route
 func Home(w http.ResponseWriter, r *http.Request) {
 	// Use r.URL.Path to check whether the request URL path exactly matches "/".
-	// If it doesn't, use the w.WriteHeader() method to send a 404 status code and
-	// the w.Write() method to write a "Not Found" response body. Importantly, we
-	// then return from the function so that the subsequent code is not executed.
+	// If it doesn't, use the http.NotFound() function to send a 404 Not Found response.
+	// Importantly, we then return from the function so that the subsequent code is not executed.
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
