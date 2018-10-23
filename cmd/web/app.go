a846ace118067fbb4096733205f4a4ada4034876
Jessica Dieuique <jessica.dieuique@gmail.com>
2018-10-23 16:43:24 -0700

    Add db to config

--- cmd/web/app.go
+++ cmd/web/app.go
@@ -1,7 +1,12 @@
 package main
 
+import (
+	"symptom-tracker/pkg/models"
+)
+
 // Define App struct to hold app wide dependencies and config settings
 type App struct {
+	Database *models.Database
 	HTMLDir string
 	StaticDir string
 }
